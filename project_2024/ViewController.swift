d4aa8d39c7a9973666ce9e502b7889f456327bfb
Jiwon Nam <skajw90@gmail.com>
2021-05-16 14:41:53 +0900

    Initial Commit

---
+++ project_2024/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project_2024
+//
+//  Created by Jiwon Nam on 5/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
